c641a992d4021ba2805c92d3135e213c1f17069a
Sven Primus <svenprimus@gmail.com>
2026-07-06 23:46:20 +0200

    render pictures

---
+++ script.js
@@ -0,0 +1,46 @@
+const pathgToGallery = "./assets/img/gallery/";
+const fotoGallery = [
+    "bochum.jpg",
+    "child_vacation.jpg",
+    "enduro_0.jpg",
+    "enduro_1.jpg",
+    "enduro_2.jpg",
+    "enduro_3.jpg",
+    "enduro_4.jpg",
+    "first_kiss.jpg",
+    "fishing_0.jpg",
+    "fishing_1.jpg",
+    "jump.jpg",
+    "lagoon.jpg",
+    "miamm.jpg",
+    "no_wacken.jpg",
+    "padel.jpg",
+    "sailing.jpg",
+];
+
+function renderGallery() {
+    const galleryRef = document.getElementById("gallery");
+    galleryRef.innerHTML = "";
+    for (let i = 0; i < fotoGallery.length; i++) {
+        galleryRef.innerHTML += /*html*/ `
+            <img class="picture" onclick="openDialog(${i})" src=${pathgToGallery + fotoGallery[i]} alt="${fotoGallery[i]}"/>
+        `;
+    }
+}
+const dialogRef = document.getElementById("pictureDialog");
+
+function openDialog(galleryIndex) {
+    // TODO cut file extension
+    // const label = fotoGallery[galleryIndex]
+
+    dialogRef.innerHTML = /*html*/ `
+        <h2></h2>
+        <img class="dialogPicture" src=${pathgToGallery + fotoGallery[galleryIndex]} alt="${fotoGallery[galleryIndex]}"/>
+        <button onclick="closeDialog()">close</button>
+    `;
+    dialogRef.showModal();
+}
+
+function closeDialog(galleryIndex) {
+    dialogRef.close();
+}
